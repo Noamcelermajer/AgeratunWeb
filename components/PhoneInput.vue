2d6077fb517376c2c72604b3466c925f96074f89
Skyfrye <noam.celermajer@gmail.com>
2023-08-21 09:28:31 -0700

    fixed contact form in schedule route

---
+++ components/PhoneInput.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="relative w-full h-[50px] py-4 bg-transparent" style="border-bottom: 1px solid #cfcfd4;">
+      <select class="absolute left-0 bg-transparent border-0 text-[#515151] w-[60px] appearance-none" v-model="selectedCode">
+        <option v-for="code in countryCodes" :key="code" :value="code">{{ code }}</option>
+      </select>
+      <input
+        type="text"
+        class="pl-[80px] text-[15px] w-full h-full text-[#515151] border-0 rounded-[4px] bg-transparent focus:outline-none focus:border-0 focus:border-black focus:shadow-none"
+        placeholder="Phone"
+        v-model="phoneNumber"
+      />
+    </div>
+</template>
+
+  
+  <script>
+  export default {
+    data() {
+      return {
+        selectedCode: '+1',
+        countryCodes: ['+1', '+44', '+33', '+972'], // Add more country codes here
+        phoneNumber: ''
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  select {
+    background-image: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 4 5"><path fill="none" stroke="#515151" d="M2 0L0 4h4z"/></svg>');
+    background-repeat: no-repeat;
+    background-position: right 5px center;
+    background-size: 12px;
+  }
+  </style>
+  
---
+++ pages/schedule.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- Header Component -->
+    <MainHeader textClass="your-text-class-if-needed" />
+
+    <!-- Contact Form (Schedule Section) -->
+    <Contact />
+
+    <!-- Footer Component -->
+    <FooterSectionTwo />
+  </div>
+</template>
+
+<script>
+import MainHeader from '@/components/header/MainHeader.vue';
+import FooterSectionTwo from '@/components/FooterSectionTwo.vue';
+import Contact from '@/components/Contact.vue';
+
+export default {
+  components: {
+    MainHeader,
+    FooterSectionTwo,
+    Contact
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  /* Add any specific styles for the Schedule page here */
+</style>
